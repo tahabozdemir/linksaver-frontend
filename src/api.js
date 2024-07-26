80363a4266999b00cc2c717a176e3c49061d3c22
Taha Bozdemir <taha@bozdemir.net>
2024-07-26 15:48:52 +0300

    fix: update API_URL

--- src/api.js
+++ src/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 import store from './redux/store'
 
+const baseURL = process.env.REACT_APP_API_URL;
 const api = axios.create({
-    baseURL: "http://localhost:8080"
+    baseURL: baseURL
 });
 
 api.interceptors.request.use((config) => {
